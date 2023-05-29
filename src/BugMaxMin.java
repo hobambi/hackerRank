97c1b9cb0b1342d63a41f20cc18639f5defe84bd
hobambi <yyunsunny@gmail.com>
2023-05-29 23:57:47 +0900

    BugMaxMin

--- src/BugMaxMin.java
+++ src/BugMaxMin.java
@@ -9,27 +9,21 @@ public class BugMaxMin {
 		String s = String.valueOf(num);
 		char[] arr = s.toCharArray();
 
-		// boolean change = false;
 		for (int i = 0; i < arr.length; i++) { // 최댓값 구하기
 			if(arr[i] != '9') {
 				maxNum = arr[i];
-				// change = true;
 				break;
 			}
 		}
 
 		String maxString = "";
 
-		for (int i = 0; i < arr.length; i++) { // 최댓값 구하기
 			maxString = s.replace(maxNum, '9');
-		}
 		max = Integer.parseInt(maxString);
 
-		// change = false;
 		for (int i = 0; i < arr.length; i++) { // 최솟값 구하기
 			if(arr[i] != '1' && i == 0) {
 				minNum = arr[0];
-				// change = true;
 				break;
 			}
 			if(arr[i] != '0' && arr[i] != arr[0]) {
